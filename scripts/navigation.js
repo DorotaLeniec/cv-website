c6067a6980f9e359559f8d6af4d98bd53c9b2e6b
dorota <dorleniec@gmail.com>
2017-02-08 16:11:21 +0100

    responsive main page and name letter animation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,26 +4,34 @@ var $menuClose = $('.menu-close');
 var $menuHamburger = $('.menu-hamburger');
 var $menuLabel = $('.sliding-menu label');
 var $currentLabel;
+var $letter = $('.name-letter')
 
-function closeSlideNavigation(){
-  $topMenu.css({'top':'0'});
-  $leftMenu.css({'left':'-1000px'});
+function closeSlideNavigation() {
+  $topMenu.css({'top': '0'});
+  $leftMenu.css({'left': '-1000px'});
   $menuLabel.css({'opacity': '0'});
 }
 
-$(document).on('change', function(){
-  $currentLabel =  $('input[name=slide]:checked').val();
-  $('#menu-pointer').html($currentLabel)
+$(document).on('change', function () {
+    $currentLabel = $('input[name=slide]:checked').val();
+    $('#menu-pointer').html($currentLabel)
   }
 )
 
-$menuHamburger.on('click', function(){
-  $topMenu.css({'top':'-100px'});
-  $leftMenu.css({'left':'0'});
-  $menuLabel.css({'opacity':'0.9'})
+$menuHamburger.on('click', function () {
+  $topMenu.css({'top': '-100px'});
+  $leftMenu.css({'left': '0'});
+  $menuLabel.css({'opacity': '0.9'})
 });
 
-$menuClose.on('click',closeSlideNavigation)
+$menuClose.on('click', closeSlideNavigation);
 
-$menuLabel.on('click', closeSlideNavigation)
+$menuLabel.on('click', closeSlideNavigation);
+
+$letter.mouseenter(function () {
+  $(this).addClass('animated rubberBand');
+  $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+    $(this).removeClass('animated rubberBand')
+  });
+})
 
